a0435c97ff21515a8f4568a6414a3ce498da8d77
Loubna <LoubnaRizqi@MacBook-Pro-de-Loubna.local>
2015-08-26 12:38:28 +0200

    Costly loop avoided

--- threshold.m
+++ threshold.m
@@ -7,20 +7,6 @@ img = rgb2gray(img);
 [m,n] = size(img);
 output = zeros(m,n);
 %% Applying the filter
-% Very naive loop on output elements
-for i=1:m
-   for j=1:n
-       if (img(i,j)<value)
-           % Setting to black pixels smaller than specified value
-           output(i,j) = 0;
-       else
-           % Setting to white pixels greater than specified value
-           output(i,j) = 1;
-       end
-   end
-end
-%% Alternative technique
-%output(find(img>value)) = 1;   
-%output(find(img<=value)) = 0;   
+output(img >= value) = 255  
 end
 
